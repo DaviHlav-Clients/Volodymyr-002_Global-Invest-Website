a257bb1a1007048b734dfdec04cd3ff586e789e1
DavidHlavacek <davi.hlav@gmail.com>
2025-02-13 13:09:38 +0100

    first push

---
+++ script.js
@@ -0,0 +1,5 @@
+// Remove all previous code and just set initial state
+document.addEventListener('DOMContentLoaded', () => {
+    const percentage = document.querySelector('.percentage');
+    percentage.textContent = '70%';
+}); 
